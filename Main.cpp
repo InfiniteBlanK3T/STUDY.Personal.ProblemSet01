d893d35a01d57f790b1662b8c4c4b37f7664a700
Thomas <vo.thomas.11647@outlook.com>
2023-03-24 01:09:51 +1100

    problem 2 last one

--- Main.cpp
+++ Main.cpp
@@ -38,65 +38,65 @@ void testProblem1()
     std::cout << M.inverse() * 45.0f << std::endl;
 }
 
-//#include <fstream>
-//
-//#include "Polygon.h"
-//
-//int testProblem2( int argc, char* argv[] )
-//{
-//    if ( argc < 2 )
-//    {
-//        std::cerr << "Arguments missing." << std::endl;
-//        std::cerr << "Usage: VectorOperations <filename>" << std::endl;
-//        
-//        // return failure, not enough arguments
-//        return 1;
-//    }
-//
-//    // create text input stream connected to file named in argv[1]
-//    std::ifstream lInput( argv[1], std::ifstream::in );
-//
-//    // operation can fail
-//    if ( !lInput.good() )
-//    {
-//        std::cerr << "Cannot open input file: " << argv[1] << std::endl;
-//        
-//        return 2;    // program failed (cannot open input)
-//    }
-//
-//    Polygon lPolygon;
-//    
-//    lPolygon.readData( lInput );
-//    
-//    // close input file
-//    lInput.close();
-//
-//    std::cout << "Signed area: " << lPolygon.getSignedArea() << std::endl;
-//    
-//    Matrix3x3 lRotate = Matrix3x3::rotate( 90.0f );
-//    
-//    Polygon lRotatedPolygon = lPolygon.transform( lRotate );
-//    
-//    std::cout << "Signed area of rotated polygon: " << lRotatedPolygon.getSignedArea() << std::endl;
-//    
-//    for ( size_t i = 0; i < lRotatedPolygon.getNumberOfVertices(); i++ )
-//    {
-//        if ( (std::round( lPolygon.getVertex( i ).dot( lRotatedPolygon.getVertex( i ) * 10.0f ) / 10.0f) != 0.0f ) )
-//        {
-//            std::cout << "Rotation error of vertex " << lPolygon.getVertex( i ) << std::endl;
-//            
-//            return 3;
-//        }
-//    }
-//
-//    std::cout << "Polygon transformation successful." << std::endl;
-//            
-//    return 0;
-//}
+#include <fstream>
+
+#include "Polygon.h"
+
+int testProblem2( int argc, char* argv[] )
+{
+    if ( argc < 2 )
+    {
+        std::cerr << "Arguments missing." << std::endl;
+        std::cerr << "Usage: VectorOperations <filename>" << std::endl;
+        
+        // return failure, not enough arguments
+        return 1;
+    }
+
+    // create text input stream connected to file named in argv[1]
+    std::ifstream lInput( argv[1], std::ifstream::in );
+
+    // operation can fail
+    if ( !lInput.good() )
+    {
+        std::cerr << "Cannot open input file: " << argv[1] << std::endl;
+        
+        return 2;    // program failed (cannot open input)
+    }
+
+    Polygon lPolygon;
+    
+    lPolygon.readData( lInput );
+    
+    // close input file
+    lInput.close();
+
+    std::cout << "Signed area: " << lPolygon.getSignedArea() << std::endl;
+    
+    Matrix3x3 lRotate = Matrix3x3::rotate( 90.0f );
+    
+    Polygon lRotatedPolygon = lPolygon.transform( lRotate );
+    
+    std::cout << "Signed area of rotated polygon: " << lRotatedPolygon.getSignedArea() << std::endl;
+    
+    for ( size_t i = 0; i < lRotatedPolygon.getNumberOfVertices(); i++ )
+    {
+        if ( (std::round( lPolygon.getVertex( i ).dot( lRotatedPolygon.getVertex( i ) * 10.0f ) / 10.0f) != 0.0f ) )
+        {
+            std::cout << "Rotation error of vertex " << lPolygon.getVertex( i ) << std::endl;
+            
+            return 3;
+        }
+    }
+
+    std::cout << "Polygon transformation successful." << std::endl;
+            
+    return 0;
+}
 
 int main( int argc, char* argv[] )
 {
     testProblem1();
 
-    //return testProblem2( argc, argv );
+    return testProblem2( argc, argv );
 }
